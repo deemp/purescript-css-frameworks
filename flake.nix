9257b0a14f792ec8b97f416d46132fb18bfefe76
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-06-05 16:09:53 +0300

    add flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs_.url = "github:deemp/flakes?dir=source-flake/nixpkgs";
+    nixpkgs.follows = "nixpkgs_/nixpkgs";
+    flake-utils_.url = "github:deemp/flakes?dir=source-flake/flake-utils";
+    flake-utils.follows = "flake-utils_/flake-utils";
+    devshell.url = "github:deemp/flakes?dir=devshell";
+    purescript-tools.url = "github:deemp/flakes?dir=language-tools/purescript";
+  };
+
+  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+      inherit (inputs.devshell.functions.${system}) mkCommands mkRunCommands mkShell;
+      packages = inputs.purescript-tools.packages.${system} // { inherit (pkgs) perl; };
+      tools = __attrValues packages;
+      devShells.default = mkShell {
+        packages = tools;
+        commands = mkCommands "tools" tools;
+      };
+    in
+    { inherit devShells; }
+  );
+}
