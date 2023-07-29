886e1310e291b7cbc290384a63341a7063714925
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-07-29 22:23:18 +0300

    upd flake

--- flake.nix
+++ flake.nix
@@ -1,25 +1,23 @@
 {
-  inputs = {
-    nixpkgs_.url = "github:deemp/flakes?dir=source-flake/nixpkgs";
-    nixpkgs.follows = "nixpkgs_/nixpkgs";
-    flake-utils_.url = "github:deemp/flakes?dir=source-flake/flake-utils";
-    flake-utils.follows = "flake-utils_/flake-utils";
-    devshell.url = "github:deemp/flakes?dir=devshell";
-    purescript-tools.url = "github:deemp/flakes?dir=language-tools/purescript";
-  };
-
-  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
-      inherit (inputs.devshell.functions.${system}) mkCommands mkRunCommands mkShell;
-      packages = inputs.purescript-tools.packages.${system} // { inherit (pkgs) perl; };
-      tools = __attrValues packages;
-      devShells.default = mkShell {
-        packages = tools;
-        bash.extra = ''export LC_ALL=C.UTF-8'';
-        commands = mkCommands "tools" tools;
+  inputs.flakes.url = "github:deemp/flakes";
+  outputs = inputs:
+    let flakes = inputs.flakes; in
+    flakes.makeFlake {
+      inputs = {
+        inherit (flakes.all) nixpkgs devshell purescript-tools;
       };
-    in
-    { inherit devShells; }
-  );
+      perSystem = { inputs, system }:
+        let
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+          inherit (inputs.devshell.lib.${system}) mkCommands mkShell;
+          packages = inputs.purescript-tools.packages.${system} // { inherit (pkgs) perl; };
+          tools = __attrValues packages;
+          devShells.default = mkShell {
+            packages = tools;
+            bash.extra = ''export LC_ALL=C.UTF-8'';
+            commands = mkCommands "tools" tools;
+          };
+        in
+        { inherit devShells; };
+    };
 }
